3ed0c56b0b026899b52fd72e01ca36c6c6030dd9
Mehrdad Sefid Dashti <git@msefid.org>
2017-08-30 14:49:50 -0400

    seems to be work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,68 +10,79 @@ var btn2Vval ;
 var btn3Val ;
 var btn4Val ;
 
-//window.onload = playGame ;
-window.onload = init ;
+var sum ;
+var msg ;
 
-function init()
+var totalWin = 0 ;
+var totalLose = 0 ;
+
+function init()   // should this function be moved down ???
 {
     secret = Math.floor(Math.random() * guessMax) + guressMin;
+    $('#sp5').text(secret) ;  //  Debug
     btn1Val = Math.floor(Math.random() * buttonNumMax) + buttonNumMin ;
     btn2Val = Math.floor(Math.random() * buttonNumMax) + buttonNumMin ;
     btn3Val = Math.floor(Math.random() * buttonNumMax) + buttonNumMin ;
     btn4Val = Math.floor(Math.random() * buttonNumMax) + buttonNumMin ;
     sum = 0 ;
     msg = '' ;
+    //$('#sp1').text(msg) ;
+
+    //totalWin = 0 ;
+    //totalLose = 0 ;
+    $('#sp1').text(msg) ;
+    $('#sp2').text('Wins:' + totalWin) ;
+    $('#sp3').text('Loses:' + totalLose) ;
+    $('#sp').text(sum) ;
 }
 
-init() ;
+$(document).ready(function()
+{
+    init() ;
 
-var val ;
-var btn ;
-var sum ;
-var msg ;
-var totalWin = 0 ;
-var totalLose = 0 ;
+    $('.redBtn').click(function()
+    {
+        sum += btn1Val ;
+        playGame() ;
+    }) ;
+    $('.blueBtn').click(function()
+    {
+        sum += btn2Val ;
+        playGame() ;
+    }) ;
+    $('.yellowBtn').click(function()
+    {
+        sum += btn3Val ;
+        playGame() ;
+    }) ;
+    $('.greenBtn').click(function()
+    {
+        sum += btn4Val ;
+        playGame() ;
+    }) ;
+    $('#sp').text(sum) ;
 
-$('.redBtn').click(function()
-{
-    sum += btn1Val ;
-    playGame() ;
-}) ;
-$('.blueBtn').click(function()
-{
-    sum += btn2Val ;
-    playGame() ;
-}) ;
-$('.yellowBtn').click(function()
-{
-    sum += btn3Val ;
-    playGame() ;
-}) ;
-$('.greenBtn').click(function()
-{
-    sum += btn4Val ;
-    playGame() ;
-}) ;
-
-function playGame()
-{
-    if (sum == secret)
+    function playGame()
     {
-        msg = 'You Won!'
-        totalWin++ ;
-        $('#sp1').text(msg) ;
-        $('#sp2').text('Wins:' + totalWin) ;
+        if (sum == secret)
+        {
+            msg = 'You Won!'
+            totalWin++ ;
+            $('#sp1').text(msg) ;
+            $('#sp2').text('Wins:' + totalWin) ;
+            init() ;
+        }
+        else if (sum > secret)
+        {
+            msg = 'You lose!'
+            totalLose++ ;
+            $('#sp1').text(msg) ;
+            $('#sp3').text('Loses:' + totalLose) ;
+            init() ;
+        }
+        else
+        {
+            $('#sp').text(sum) ;
+        }
     }
-    else if (sum > secret)
-    {
-        msg = 'You lose!'
-        totalLose++ ;
-        $('#sp1').text(msg) ;
-        $('#sp2').text('Loses:' + totalLose) ;
-    }
-    else
-    {
-        $('#sp').text(sum) ;
-    }
-}
+});
